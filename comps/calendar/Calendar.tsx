3275abb5db7f5ac67720c46e026f4b9f9828faa9
asi-isa <ali.inceoglu@protonmail.com>
2022-04-01 09:22:44 +0200

    cal init

---
+++ comps/calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./Calendar.module.css";
+
+export default function Calendar() {
+  return <section className={styles.con}></section>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Calendar from "../comps/calendar/Calendar";
 import Habits from "../comps/habits/Habits";
 import styles from "../styles/Home.module.css";
 
@@ -9,9 +10,9 @@ export default function Home() {
       <div className={styles.pattern_3} />
 
       <div className={styles.inner_con}>
-        <div className={styles.habits_con}>
-          <Habits />
-        </div>
+        <Habits />
+
+        <Calendar />
       </div>
     </section>
   );
